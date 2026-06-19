3b32e369d9f41e259b4c6ab43848ec834e3b10f8
高木桜子 <sakurakotakaki@takagisakurakonoMacBook-Air.local>
2026-06-20 00:57:53 +0900

    ログイン画面、新規登録画面、ダッシュボードのサイドバーのレスポンシブ実装

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # Devise（ログイン・登録・ログアウト）
   devise_for :users, controllers: { sessions: "users/sessions", registrations: "users/registrations" }
 
-  # ゲストログイン（devise_scope で囲む必要がある）
+  # ゲストログイン（devise_scope)
   devise_scope :user do
     post "guest_sign_in", to: "users/sessions#guest_sign_in"
   end
